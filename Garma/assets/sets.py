20b89f4454dc08c21b78b64e2bbc7093fdddc3f1
Jakob Roxas <112476618+JakobRoxas@users.noreply.github.com>
2024-03-10 23:47:56 +0800

    Add files via upload

---
+++ Garma/assets/sets.py
@@ -0,0 +1,131 @@
+Capitals = [
+    {
+        "question": "What is the capital of France?",
+        "answer": "Paris",
+        "front_image": f"/images/good_score.jpg",
+        "back_image": f"/images/bad_score.jpg"
+    },
+    {
+        "question": "What is the capital of Spain?",
+        "answer": "Madrid",
+        "front_image": f"/images/garma.jpg",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "What is the capital of Germany?",
+        "answer": "Berlin",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "What is the capital of Italy?",
+        "answer": "Rome",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "What is the capital of Portugal?",
+        "answer": "Lisbon",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    }
+]
+
+GTA = [
+    {
+        "question": "What is the release year of GTA V?",
+        "answer": "2013",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "What is the release year of GTA IV?",
+        "answer": "2008",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "What is the release year of GTA San Andreas?",
+        "answer": "2004",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "What is the release year of GTA Vice City?",
+        "answer": "2002",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "What is the release year of GTA III?",
+        "answer": "2001",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    }
+]
+
+Persona = [
+    {
+        "question": "When was the release year of Persona 5?",
+        "answer": "2016",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "When was the release year of Persona 4?",
+        "answer": "2008",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "When was the release year of Persona 3?",
+        "answer": "2006",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "When was the release year of Persona 2?",
+        "answer": "1999",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "When was the release year of Persona 1?",
+        "answer": "1996",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    }
+]
+
+Pokemon = [
+    {
+        "question": "What is the first Pokemon in the Pokedex?",
+        "answer": "Bulbasaur",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "What is the second Pokemon in the Pokedex?",
+        "answer": "Ivysaur",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "What is the third Pokemon in the Pokedex?",
+        "answer": "Venusaur",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "What is the fourth Pokemon in the Pokedex?",
+        "answer": "Charmander",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    },
+    {
+        "question": "What is the fifth Pokemon in the Pokedex?",
+        "answer": "Charmeleon",
+        "front_image": "https://picsum.photos/380/380",
+        "back_image": "https://picsum.photos/380/380"
+    }
+]
